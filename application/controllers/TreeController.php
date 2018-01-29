1e2abd4299d75b311a4d13b3060d8dcc09fe21c6
plsatin <pslater.ru@gmail.com>
2018-01-29 14:16:05 +0500

    Version: 0.3.2

--- application/controllers/TreeController.php
+++ application/controllers/TreeController.php
@@ -26,49 +26,6 @@ class TreeController extends MonitoringAwareController
         
     }
 
-    // public function oldAction()
-    // {
-
-    //     $this->view->controller = $this;
-
-    //     $this->view->treehost = $treehost = $this->getRequest()->getUrl()->getParam('host');
-    //     if ($treehost != null) {
-    //         $this->view->r_Win32_OperatingSystem = $r_Win32_OperatingSystem = DbObject::getWmiClass($treehost, 8);
-    //         $this->view->r_Win32_ComputerSystem = $r_Win32_ComputerSystem = DbObject::getWmiClass($treehost, 2);
-    //         $this->view->r_Win32_BaseBoard = $r_Win32_BaseBoard = DbObject::getWmiClass($treehost, 16);
-    //         $this->view->r_Win32_BIOS = $r_Win32_BIOS = DbObject::getWmiClass($treehost, 1);
-    //         $this->view->r_Win32_Processor = $r_Win32_Processor = DbObject::getWmiClass($treehost, 11);
-    //         $this->view->r_Win32_PhysicalMemoryArray = $r_Win32_PhysicalMemoryArray = DbObject::getWmiClass($treehost, 28);
-    //         $this->view->r_Win32_PhysicalMemory = $r_Win32_PhysicalMemory = DbObject::getWmiClass($treehost, 15);
-    //         $this->view->r_Win32_DiskDrive = $r_Win32_DiskDrive = DbObject::getWmiClass($treehost, 4);
-    //         $this->view->r_Win32_LogicalDisk = $r_Win32_LogicalDisk = DbObject::getWmiClass($treehost, 5);
-    //         $this->view->r_Win32_IDEController = $r_Win32_IDEController = DbObject::getWmiClass($treehost, 17);
-    //         $this->view->r_Win32_SCSIController = $r_Win32_SCSIController = DbObject::getWmiClass($treehost, 18);
-    //         $this->view->r_Win32_USBController = $r_Win32_USBController = DbObject::getWmiClass($treehost, 19);
-    //         $this->view->r_Win32_USBHub = $r_Win32_USBHub = DbObject::getWmiClass($treehost, 20);
-    //         $this->view->r_Win32_PointingDevice = $r_Win32_PointingDevice = DbObject::getWmiClass($treehost, 21);
-    //         $this->view->r_Win32_Keyboard = $r_Win32_Basr_Win32_KeyboardeBoard = DbObject::getWmiClass($treehost, 22);
-    //         $this->view->r_Win32_SerialPort = $r_Win32_SerialPort = DbObject::getWmiClass($treehost, 23);
-    //         $this->view->r_Win32_ParallelPort = $r_Win32_ParallelPort = DbObject::getWmiClass($treehost, 24);
-    //         $this->view->r_Win32_NetworkAdapter = $r_Win32_NetworkAdapter = DbObject::getWmiClass($treehost, 6);
-    //         $this->view->r_Win32_NetworkAdapterConfiguration = $r_Win32_NetworkAdapterConfiguration = DbObject::getWmiClass($treehost, 7);
-    //         $this->view->r_Win32_VideoController = $r_Win32_VideoController = DbObject::getWmiClass($treehost, 13);
-    //         $this->view->r_Win32_DesktopMonitor = $r_Win32_DesktopMonitor = DbObject::getWmiClass($treehost, 3);
-    //         $this->view->r_Win32_SoundDevice = $r_Win32_SoundDevice = DbObject::getWmiClass($treehost, 12);
-    //         $this->view->r_Win32_Printer = $r_Win32_Printer = DbObject::getWmiClass($treehost, 10);
-
-
-
-
-    //     } else {
-    //         echo 'The parameter is not set ?host';
-    //     }
-
-
-    //     //$this->view->tabs = $this->tabs()->activate('tree');
-
-    // }
-
     public function indexAction()
     {
 
